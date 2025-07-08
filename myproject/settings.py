a4555c0148493c0158039463647f3cbd94309117
Alicia <chegealicia8@gmail.com>
2025-07-08 23:41:36 +0300

    changing url

--- myproject/settings.py
+++ myproject/settings.py
@@ -86,13 +86,16 @@ MIDDLEWARE = [
 CORS_ALLOW_CREDENTIALS = True  
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173", 
-    "http://localhost:5174"
+    "http://localhost:5174",
+    "https://hr-frontend-nbkexoywm-lisha183s-projects.vercel.app"
 ]
 # CORS_ALLOW_ALL_ORIGINS = True
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173", 
     "http://127.0.0.1:8000",
-    "http://localhost:5174"
+    "http://localhost:5174",
+    "https://hr-frontend-nbkexoywm-lisha183s-projects.vercel.app"
+
 
 ]
 
