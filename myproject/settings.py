147aa506ab97e46705a38b95b288648e2d8aec4b
Alicia <chegealicia8@gmail.com>
2025-08-20 13:39:16 +0300

    urls

--- myproject/settings.py
+++ myproject/settings.py
@@ -135,7 +135,8 @@ CORS_ALLOWED_ORIGINS = [
     "https://hr-frontend-us1v-hwe5y4t3r-lisha183s-projects.vercel.app",
     "https://hr-frontend-us1v-8xwxu6z88-lisha183s-projects.vercel.app",
     "https://hr-frontend-us1v-lngxs48pv-lisha183s-projects.vercel.app",
-    "https://hr-frontend-us1v-bt4aly9to-lisha183s-projects.vercel.app"
+    "https://hr-frontend-us1v-bt4aly9to-lisha183s-projects.vercel.app",
+    "https://hr-frontend-us1v-7trkaxvh4-lisha183s-projects.vercel.app"
 
 
 ]
@@ -160,7 +161,8 @@ CSRF_TRUSTED_ORIGINS = [
     "https://hr-frontend-us1v-hwe5y4t3r-lisha183s-projects.vercel.app",
     "https://hr-frontend-us1v-8xwxu6z88-lisha183s-projects.vercel.app",
     "https://hr-frontend-us1v-lngxs48pv-lisha183s-projects.vercel.app",
-    "https://hr-frontend-us1v-bt4aly9to-lisha183s-projects.vercel.app"
+    "https://hr-frontend-us1v-bt4aly9to-lisha183s-projects.vercel.app",
+    "https://hr-frontend-us1v-7trkaxvh4-lisha183s-projects.vercel.app"
 
   
 ]
