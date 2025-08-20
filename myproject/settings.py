b8a61af6c4a3f31f2e0ba60bcfd0ae1b07dcc20c
Alicia <chegealicia8@gmail.com>
2025-08-20 12:53:41 +0300

    adding urls

--- myproject/settings.py
+++ myproject/settings.py
@@ -133,6 +133,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://hr-frontend-o6tb.vercel.app",
     "https://hr-frontend-nine.vercel.app",
     "https://hr-frontend-us1v-hwe5y4t3r-lisha183s-projects.vercel.app",
+    "https://hr-frontend-us1v-8xwxu6z88-lisha183s-projects.vercel.app"
 
 
 ]
@@ -155,6 +156,7 @@ CSRF_TRUSTED_ORIGINS = [
     "https://hr-frontend-o6tb.vercel.app",
     "https://hr-frontend-nine.vercel.app",
     "https://hr-frontend-us1v-hwe5y4t3r-lisha183s-projects.vercel.app",
+    "https://hr-frontend-us1v-8xwxu6z88-lisha183s-projects.vercel.app"
 
   
 ]
